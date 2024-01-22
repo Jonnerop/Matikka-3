201022be4e1a9014436408a2db3892d64376717a
Jonnerop <142784165+Jonnerop@users.noreply.github.com>
2024-01-22 16:39:59 +0200

    Create tehtävä 1.py
    tehtävä 1 ratkaisu

---
+++ tehtävä 1.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+#satunnaisesti generoitu 5-alkioinen kokonaislukutaulukko (lukuvälin ylärajaksi laitoin 100)
+
+random_array = np.random.randint(0, 100, 5)
+print(random_array)
